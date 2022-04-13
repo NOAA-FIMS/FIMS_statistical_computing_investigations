18a747e8a084714fa1f9a38939ea0906ec1dcb9b
msupernaw <matthew.supernaw@noaa.gov>
2022-04-13 16:50:51 -0400

    Update functional_analysis.hpp

--- include/functional_analysis.hpp
+++ include/functional_analysis.hpp
@@ -28,21 +28,56 @@ public:
 
     }
 
-    void combinations_builder(int& count, int current, std::vector<T>& working, std::vector<std::vector<T> >& source, std::vector<std::vector<T> >& combos) {
+    void Analyze() {
+
+        this->input_values.resize(this->parameters.size());
+        for (int i = 0; i < this->parameters.size(); i++) {
+            for (T v = this->parameters[i]->GetMinBoundary(); v <= this->parameters[i]->GetMaxBoundary(); v += delta) {
+
+                input_values[i].push_back(v);
+            }
+        }
+
+        std::vector<T> working(this->parameters.size());
+
+        int count = 0;
+        int current = 0;
+        this->argument_builder(count, current, working, input_values, argument_values);
+
+
+
+        //infentesimal step
+        for (int i = 0; i < argument_values.size(); i++) {
+            this->UpdateParameterSet(argument_values[i]);
+
+        }
+
+
+
+        //values
+
+        //derivatives
+
+
+    }
+
+private:
+
+    void argument_builder(int& count, int current, std::vector<T>& working, std::vector<std::vector<T> >& source, std::vector<std::vector<T> >& combos) {
 
 
         if (current == 0) {
             for (int i = 0; i < source[0].size(); i++) {
                 std::vector<T> v;
                 v.push_back(source[0][i]);
-                combinations_builder(count, current + 1, v, source, combos);
+                argument_builder(count, current + 1, v, source, combos);
             }
         } else if (current < source.size() - 1) {
             std::vector<T> v = working;
-//            v.push_back(0);
+            //            v.push_back(0);
             for (int i = 0; i < source[current].size(); i++) {
                 v[v.size() - 1] = source[current][i];
-                combinations_builder(count, current + 1, v, source, combos);
+                argument_builder(count, current + 1, v, source, combos);
             }
         } else {
 
@@ -57,46 +92,11 @@ public:
         }
     }
 
-    void Analyze() {
-
-        this->input_values.resize(this->parameters.size());
-        for (int i = 0; i < this->parameters.size(); i++) {
-            for (T v = this->parameters[i]->GetMinBoundary(); v <= this->parameters[i]->GetMaxBoundary(); v += delta) {
-
-                input_values[i].push_back(v);
-            }
+    void UpdateParameterSet(std::vector<T>& v) {
+        for (int i = 0; i < v.size(); i++) {
+            this->parameters[i]->SetValue(v[i]);
         }
-
-        std::vector<T> working(this->parameters.size());
-    
-        int count = 0;
-        int current = 0;
-        this->combinations_builder(count, current, working, input_values, argument_values);
-
-
-        
-        //infentesimal step
-        for (int i = 0; i < argument_values.size(); i++) {
-           this->UpdateParameterSet(argument_values[i]);
-
-        }
-
-        
-
-        //values
-
-        //derivatives
-
-
     }
-    
-    private:
-        
-        void UpdateParameterSet(std::vector<T>& v){
-            for(int i =0; i < v.size(); i++){
-                this->parameters[i]->SetValue(v[i]);
-            }
-        }
 
 
 };
