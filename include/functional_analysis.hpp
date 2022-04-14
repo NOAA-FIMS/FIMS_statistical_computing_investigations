7d4f0bd553f4574241a378ab9a5805286fc480bf
msupernaw <matthew.supernaw@noaa.gov>
2022-04-14 10:26:48 -0400

    Update functional_analysis.hpp

--- include/functional_analysis.hpp
+++ include/functional_analysis.hpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <limits>
 
+#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+#define PBWIDTH 60
+
 template<typename T>
 class FunctionalAnalysis {
 public:
@@ -17,8 +20,8 @@ public:
     std::vector<std::vector<T> > covariance;
     std::vector<std::vector<T> > correlation;
     bool is_continuous = false;
-    T min_value = std::limits<T>::max();
-    T max_value = std::limits<T>::max();
+    T min_value = std::numeric_limits<T>::max();
+    T max_value = std::numeric_limits<T>::max();
     std::vector<T> argument_min;
     std::vector<T> argument_max;
 
@@ -48,16 +51,17 @@ public:
 
         int count = 0;
         int current = 0;
-        this->argument_builder(count, current, working, input_values, argument_values);
+        this->ArgumentBuilder(count, current, working, input_values, argument_values);
         std::cout << "done.\n";
 
 
         //Run Infinitesimal step - Riemann
         for (int i = 0; i < argument_values.size(); i++) {
 
-            std::cout << (i + 1) << " of " << argument_values.size() << " (" << (((double) i / (double) argument_values.size())*100) << "%)" << std::endl;
+//            std::cout << (i + 1) << " of " << argument_values.size() << " (" << (((double) i / (double) argument_values.size())*100) << "%)" << std::endl;
             this->UpdateParameterSet(argument_values[i]);
 
+            this->Progress(((double) i / (double) argument_values.size()));
             //reset the tape
             Variable::tape.Reset();
 
@@ -81,13 +85,13 @@ public:
             Variable::tape.AccumulateFirstOrder();
 
 
-            std::cout << "Function value: " << v << "\nGradient:\n";
+//            std::cout << "Function value: " << v << "\nGradient:\n";
             for (int p = 0; p< this->parameters.size(); p++) {
-                std::cout << Variable::tape.Value(this->parameters[p]->info->id) << "  ";
+//                std::cout << Variable::tape.Value(this->parameters[p]->info->id) << "  ";
                 //store derivatives
                 this->derivatives[this->parameters[p]->info->id].push_back(Variable::tape.Value(this->parameters[p]->info->id));
             }
-            std::cout << "\n\n" << std::flush;
+//            std::cout << "\n\n" << std::flush;
 
         }
 
@@ -105,21 +109,21 @@ public:
 
 private:
 
-    void argument_builder(int& count, int current, std::vector<T>& working, std::vector<std::vector<T> >& source, std::vector<std::vector<T> >& combos) {
+    void ArgumentBuilder(int& count, int current, std::vector<T>& working, std::vector<std::vector<T> >& source, std::vector<std::vector<T> >& combos) {
 
 
         if (current == 0) {
             for (int i = 0; i < source[0].size(); i++) {
                 std::vector<T> v;
                 v.push_back(source[0][i]);
-                argument_builder(count, current + 1, v, source, combos);
+                ArgumentBuilder(count, current + 1, v, source, combos);
             }
         } else if (current < source.size() - 1) {
             std::vector<T> v = working;
             //            v.push_back(0);
             for (int i = 0; i < source[current].size(); i++) {
                 v[v.size() - 1] = source[current][i];
-                argument_builder(count, current + 1, v, source, combos);
+                ArgumentBuilder(count, current + 1, v, source, combos);
             }
         } else {
 
@@ -140,6 +144,13 @@ private:
         }
     }
 
+    void Progress(double percentage) {
+        int val = (int) (percentage * 100);
+        int lpad = (int) (percentage * PBWIDTH);
+        int rpad = PBWIDTH - lpad;
+        printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
+        fflush(stdout);
+    }
 
 };
 
